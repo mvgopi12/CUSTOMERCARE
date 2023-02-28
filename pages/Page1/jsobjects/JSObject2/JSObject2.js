f7c220c178372c9ef6078f1359d206999c7c8e39
Venu gopi <mvgopi@lustralwater.com>
2023-02-28 05:47:03 +0000

    logs updated

--- pages/Page1/jsobjects/JSObject2/JSObject2.js
+++ pages/Page1/jsobjects/JSObject2/JSObject2.js
@@ -18,6 +18,27 @@ export default {
 	 }
 		return data
 	},
+	returnName :()=>{
+		if(Tabs1.selectedTab == "DAILY LEADS"){
+			return Text9.text
+		}else if(Tabs1.selectedTab == "ADD CUSTOMER"){
+			return A_Name.text
+		}
+	},
+	returnPH :()=>{
+		if(Tabs1.selectedTab == "DAILY LEADS"){
+			return L_NUM.text
+		}else if(Tabs1.selectedTab == "ADD CUSTOMER"){
+			return A_NUM.text
+		}
+	},
+	returnRemarks :()=>{
+		if(Tabs1.selectedTab == "DAILY LEADS"){
+			return L_REMARK.selectedOptionLabel
+		}else if(Tabs1.selectedTab == "ADD CUSTOMER"){
+			return A_REMARK.selectedOptionLabel
+		}
+	},
 	myFun2: async () => {
 		//use async-await or promises
 	}
