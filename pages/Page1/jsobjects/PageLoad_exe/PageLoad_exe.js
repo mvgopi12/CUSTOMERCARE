d1597b9ac077c438210f4507a1434488eddc46b1
Venu gopi <mvgopi@lustralwater.com>
2023-03-03 05:21:48 +0000

    scheduled function run on page load

--- pages/Page1/jsobjects/PageLoad_exe/PageLoad_exe.js
+++ pages/Page1/jsobjects/PageLoad_exe/PageLoad_exe.js
@@ -2,15 +2,15 @@ export default {
 	onPageLoad: async () => {
 		//use async-await or promises
 		setTimeout(function(){
-        setInterval(() => {JSObject2.SCHD(),SCHD_CALLS.run()}, 10000, "noPageLoadRepeatTimer");
+        setInterval(() => {JSObject2.SCHD()}, 10000, "noPageLoadRepeatTimer");
     }, 22000);
 		return "success";
 	},
-	// Schedduled: async () => {
-		// //use async-await or promises
-		// setTimeout(function(){
-        // setInterval(() => {SCHD_CALLS.run() }, 10000, "noPageLoadRepeatTimer");
-    // }, 22000);
-		// return "success";
-	// }
+	Schedduled: async () => {
+		//use async-await or promises
+		setTimeout(function(){
+        setInterval(() => {SCHD_CALLS.run() }, 10000, "noPageLoadRepeatTimer");
+    }, 22000);
+		return "success";
+	}
 }
