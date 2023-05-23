8d9128db54817458fbef69440763b0e98fdc10c7
Venu gopi <mvgopi@lustralwater.com>
2023-05-23 05:45:05 +0000

    followups CUSTOMER WILL CALLBACK added

--- pages/Page1/jsobjects/S_LEADS/S_LEADS.js
+++ pages/Page1/jsobjects/S_LEADS/S_LEADS.js
@@ -14,13 +14,13 @@ export default {
 	},
 	SCHD :()=>{
 		let data=[];
-	for (let i=0; i<SCHD_CALLS.data.length; i++)
-	{	
-		if(	(SCHD_CALLS.data[i].SCHEDULE_DATE)<=DATE_FORMAT.get_today_with_minutes())
-		{
-			data.push(SCHD_CALLS.data[i]);
+		for (let i=0; i<SCHD_CALLS.data.length; i++)
+		{	
+			if(	(SCHD_CALLS.data[i].SCHEDULE_DATE)<=DATE_FORMAT.get_today_with_minutes())
+			{
+				data.push(SCHD_CALLS.data[i]);
+			}
 		}
-	}
 		data.sort((a,b)=>{
 			return new Date(b.SCHEDULE_DATE).getTime()  - new Date(a.SCHEDULE_DATE).getTime();
 		});
@@ -30,7 +30,7 @@ export default {
 		let data=[];
 		for (let i=0; i<LEADS.data.length; i++)
 		{
-			if(LEADS.data[i].REMARK=="INTERESTED" || LEADS.data[i].REMARK=="SWITCH OFF/NOT REACHBLE" || LEADS.data[i].REMARK=="CUSTOMER DISCONNECTING" && LEADS.data[i].ACTIVE==true)
+			if(LEADS.data[i].REMARK=="INTERESTED" || LEADS.data[i].REMARK=="SWITCH OFF/NOT REACHBLE" || LEADS.data[i].REMARK=="CUSTOMER DISCONNECTING" || LEADS.data[i].REMARK=="CUSTOMER WILL CALLBACK" && LEADS.data[i].ACTIVE==true)
 			{
 				data.push(LEADS.data[i]);
 			}
@@ -38,15 +38,15 @@ export default {
 		return data;
 	},
 	// Old_Leads:()=>{
-		// let returnData=[];
-		// for (let i= (LEADS.data.length-1); i>=0;i--)
-		// {
-			// if(LEADS.data[i].REMARK== "OLD LEADS" && LEADS.data[i].ACTIVE==true)
-			// {
-				// returnData.push(LEADS.data[i])
-			// }
-		// }
-		// return returnData
+	// let returnData=[];
+	// for (let i= (LEADS.data.length-1); i>=0;i--)
+	// {
+	// if(LEADS.data[i].REMARK== "OLD LEADS" && LEADS.data[i].ACTIVE==true)
+	// {
+	// returnData.push(LEADS.data[i])
+	// }
+	// }
+	// return returnData
 	// },
 	Referral_Data :()=>{
 		let data =[];
@@ -90,29 +90,29 @@ export default {
 		}
 	},
 	UPDATE_Popup :()=>{
-			if(appsmith.user.username == "sheela@lustralwater.com" || appsmith.user.username == "mvgopi@lustralwater.com" || appsmith.user.username == "support@lustralwater.com"){
-				popUp_Update.run()
-				Sheela_Popup.run()
-			}else if(appsmith.user.username == "sindhuvaspar@gmail.com"){
-				popUp_Update.run()
-				Sindhu_Popup.run()
-			}else if(appsmith.user.username == "a.shet@lustralwater.com"){
-				popUp_Update.run()
-				Aishwarya_Popup.run()
-			}
+		if(appsmith.user.username == "sheela@lustralwater.com" || appsmith.user.username == "mvgopi@lustralwater.com" || appsmith.user.username == "support@lustralwater.com"){
+			popUp_Update.run()
+			Sheela_Popup.run()
+		}else if(appsmith.user.username == "sindhuvaspar@gmail.com"){
+			popUp_Update.run()
+			Sindhu_Popup.run()
+		}else if(appsmith.user.username == "a.shet@lustralwater.com"){
+			popUp_Update.run()
+			Aishwarya_Popup.run()
+		}
 	},
 	A_CUSTOMER :()=>{
 		if(A_Name.text !="" && A_NUM.text != ""){
 			if(appsmith.user.username == "sheela@lustralwater.com" || appsmith.user.username == "mvgopi@lustralwater.com" || appsmith.user.username == "support@lustralwater.com"){
-			C_INSERT.run()
+				C_INSERT.run()
 				Sheela_Logs.run()	
 				showModal("LEAD_ADDED_Success")
 			}else if(appsmith.user.username == "sindhuvaspar@gmail.com"){
-			C_INSERT.run()
+				C_INSERT.run()
 				Sindhu_Logs.run()
 				showModal("LEAD_ADDED_Success")
 			}else if(appsmith.user.username == "a.shet@lustralwater.com"){
-			C_INSERT.run()
+				C_INSERT.run()
 				Aishwarya_Logs.run()
 				showModal("LEAD_ADDED_Success")
 			}
@@ -161,6 +161,6 @@ export default {
 		}
 	},
 	myFun2: async () => {
-			
+
 	}
 }
